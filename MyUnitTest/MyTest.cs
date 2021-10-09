8f5af632819e38590e73657341df603f17c691ae
ElizarovRoman <77525760+ElizarovRoman@users.noreply.github.com>
2021-10-09 15:22:57 +0400

    Add files via upload

---
+++ MyUnitTest/MyTest.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyUnitTest
+{
+    public class MyTest
+    {
+        public int multiplication(int x, int u)
+        {
+            return x * u;
+        }
+    }
+}
